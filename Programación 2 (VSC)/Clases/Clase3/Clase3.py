5c4aec6870ee3d6a0826f371f9b928fbe805e0c2
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-09-01 08:18:47 -0300

    Update Clase3.py

--- Programación 2 (VSC)/Clases/Clase3/Clase3.py
+++ Programación 2 (VSC)/Clases/Clase3/Clase3.py
@@ -8,8 +8,26 @@ import math
 import datetime
 
 def main():
-    pass
-    
+    print("Recorre letras")
+    for i in 'aieou': #
+        print(i)
+
+    print("Recorre colección de números")
+    for i in range(5): #range() crea una colección de 5 numeros, va de 0 a 4
+        print(i)
+
+    print("For para adelante")
+    # int i=0; i<5; i++    
+    for i in range(0,5): #indico el inicio de la colección, final de la colección
+        print(i)
+
+    print("For para atrás")
+    # int i=0; i>5; i--    
+    for i in range(5,0,-1): #(indico el inicio de la colección, final de la colección, y el sentido)
+        print(i)
+
+
+
 
 
 if __name__=="__main__":
