d75198f6958d6b3188b64717fd0ae31b39505329
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-09-14 09:29:42 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,3 +1,21 @@
+'''
+Permita llenar una tupla de python con valores enteros impares entre 0 y 1000. La acción de llenar debe implementarse en una función debe tener un argumneto de entrada y como valor de retorno la misma tupla
+
+La tupla no es dinámica, no puedo tener una funcion q le pase cosas a la tupla. pero si puedo tener una lista y la lista la paso a tupla
+'''
+
+def main():
+    Q,R
+    K=1   
+    Q=K/2
+    R=K-(Q*2)
+    if(R==0):
+
+
+if __name__=="__main__":
+    main()
+
+
 import datetime
 
 i=1
