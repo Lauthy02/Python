18cb2e5141b063155781ea2d72b36ee82ef2e4df
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-11-23 13:24:04 -0300

    2do parcial

---
+++ Programación 2 (VSC)/Clases/Clase15/Parcial2/Clase15-Parcial2.py
@@ -0,0 +1,8 @@
+import os
+import lib.Clases as cta
+
+def main():
+    pass
+
+if(__name__=="__main__"):
+    main()
---
+++ Programación 2 (VSC)/Clases/Clase15/Parcial2/lib/Clases.py
@@ -0,0 +1,14 @@
+class Tablero:
+    pass
+
+    def __init__(self):
+        pass
+
+    def generarPalabraOculta(self):
+        pass
+
+    def dibujarTablero(self):
+        pass
+
+    def existeLetra(self, letra):
+        pass
