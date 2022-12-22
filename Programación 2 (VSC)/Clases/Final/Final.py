500bdf9bc0c48c1c04a6df0aabeab1739092b02c
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-12-22 06:24:13 -0300

    Final

---
+++ Programación 2 (VSC)/Clases/Final/Final.py
@@ -0,0 +1,15 @@
+'''
+---- Evaluacón Final de Programación 2 (Python) ----
+Alumno: Rojas Lautaro
+Fecha: 22/12/2022
+Tema 
+Localizacón: Centro (San Juan)
+'''
+import os
+import lib.Clases as clas
+
+def main():
+    pass
+
+if(__name__=="__main__"):
+    main()
